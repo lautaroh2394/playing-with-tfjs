cec90124c7e8b0b8030a8ba2074f6220b1c6f6bb
lautaroh2394 <lautarohaase_23@hotmail.com>
2023-06-08 20:52:19 -0300

    constant analysis feature

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import { startStream } from "./start-stream";
+
 let draws = 0;
+const WIDTH = 300
+const HEIGHT = 243
+
 const detectorConfig = {
   architecture: 'MobileNetV1',
   outputStride: 16,
-  inputResolution: { width: 300, height: 243 },
+  inputResolution: { width: WIDTH, height: HEIGHT },
   multiplier: 0.75,
   quantBytes: 2,
 };
@@ -30,43 +34,13 @@ const drawPoint = (ctx, keypoint)=>{
   ctx.stroke();
 }
 
-analize.addEventListener("click", async ()=>{
-    const context = pictureCanvas.getContext("2d");
-    pictureCanvas.width = 300;
-    pictureCanvas.height = 243;
-    imgTaken.setAttribute("width", pictureCanvas.width);
-    imgTaken.setAttribute("height", pictureCanvas.height);
-
-    context.drawImage(video, 0, 0, pictureCanvas.width, pictureCanvas.height);
-    const data = pictureCanvas.toDataURL("image/png");
-    imgTaken.setAttribute("src", data);
-})
-
-imgTaken.addEventListener('load', async ()=>{
-  //how to do this for every frame
-  pictureCanvas.height = imgTaken.height
-  pictureCanvas.width = imgTaken.width
-
-  const [poses] = await detector.estimatePoses(imgTaken);
-  console.log(poses)
-  drawPoints(pictureCanvas, poses.keypoints)
-})
-
 const constantAnalisis = async ()=>{
-  constantAnalisisCanvas.height = 243
-  constantAnalisisCanvas.width = 300
+  constantAnalisisCanvas.height = HEIGHT
+  constantAnalisisCanvas.width = WIDTH
   const canvasToDrawPic = document.createElement('canvas')
-  canvasToDrawPic.height = 243
-  canvasToDrawPic.width = 300
-  /*
-  const img = document.createElement('img')
-  img.addEventListener('load', async ()=>{
-    const [poses] = await detector.estimatePoses(img);
-    console.log(poses)
-    drawPoints(constantAnalisisCanvas, poses.keypoints)
-    requestAnimationFrame(analize)
-  })
-  */
+  canvasToDrawPic.height = HEIGHT
+  canvasToDrawPic.width = WIDTH
+
   setInterval(()=>{
     console.log(`${draws} per second`)
     draws = 0;
@@ -81,14 +55,7 @@ const constantAnalisis = async ()=>{
 
   let poses;
   const analize = async ()=>{
-    /*
-    const context = canvasToDrawPic.getContext("2d");
-    context.drawImage(video, 0, 0, canvasToDrawPic.width, canvasToDrawPic.height);
-    const data = canvasToDrawPic.toDataURL("image/png");
-    img.setAttribute("src", data);  
-    */
     [poses] = await detector.estimatePoses(video, estimationConfig);
-    //console.log(poses)
     drawPoints(constantAnalisisCanvas, poses.keypoints)
     requestAnimationFrame(analize)
   }
@@ -96,48 +63,10 @@ const constantAnalisis = async ()=>{
   requestAnimationFrame(analize)
 }
 
-/*
-const constantAnalisisV2 = async ()=>{
-  //todo -- fix. a bit too slow. maybe not show video and show each take?
-  video.style.display = 'none'
-  constantAnalisisCanvasTwo.height = 243
-  constantAnalisisCanvasTwo.width = 300
-  constantAnalisisCanvasThree.height = 243
-  constantAnalisisCanvasThree.width = 300
-
-  const canvasToDrawPic = constantAnalisisCanvasTwo
-  const img = document.createElement('img')
-  img.addEventListener('load', async ()=>{
-    const [poses] = await detector.estimatePoses(img);
-    console.log(poses)
-    drawPoints(constantAnalisisCanvas, poses.keypoints)
-    requestAnimationFrame(analize)
-  })
-  const analize = async ()=>{
-    
-    const context = canvasToDrawPic.getContext("2d");
-    context.drawImage(video, 0, 0, canvasToDrawPic.width, canvasToDrawPic.height);
-    const data = canvasToDrawPic.toDataURL("image/png");
-    img.setAttribute("src", data);  
-    
-    const [poses] = await detector.estimatePoses(img);
-    console.log(poses)
-    drawPoints(constantAnalisisCanvasThree, poses.keypoints)
-    requestAnimationFrame(analize)
-  }
-  
-  requestAnimationFrame(analize)
-}
-*/
 
 constantAnalisisButton.addEventListener('click', ()=>{
   constantAnalisis()
 })
-/*
-constantAnalisisV2Button.addEventListener('click', ()=>{
-  constantAnalisisV2()
-})
-*/
 
 video.style.display = 'none'
 constantAnalisisButton.style.display = 'none'
